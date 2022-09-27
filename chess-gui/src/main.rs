d72970c0d0abdcbc47d5f4d4911d2b16574fa35d
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2022-09-27 11:42:46 +0200

    Try to import chess lib

--- chess-gui/src/main.rs
+++ chess-gui/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut b : Board_state = create_init_board();
+    create_start_config(&b);
+    for row in b._board_types {
+        for cell in row: {
+            print!("|{}", get_symbol(cell));
+        }
+        println!("|");
+    }
+}
+
+fn get_symbol(cell: Cell) -> &str {
+    match cell {
+        Cell::King => "k",
+        Cell::Queen => "q",
+        Cell::Bishop => "b",
+        Cell::Knight => "h",
+        Cell::Rook => "r",
+        Cell::Pawn => "p"
+        Cell::None => "_"
+    }
 }
